721cd85005ee9d057986b0ceab24de4a7d8a94dd
Cecilia Sanare <ceci@sanare.dev>
2024-03-06 18:27:00 -0600

    feat: reworked visuals of the app image cards

--- src/components/AppImage.tsx
+++ src/components/AppImage.tsx
@@ -1,6 +1,7 @@
 import { type FC } from 'react';
 import { Link } from 'react-router-dom';
 import { cn } from '../utils/cn';
+import * as styles from './AppImage.module.css';
 
 type Props = {
   className?: string;
@@ -10,16 +11,13 @@ type Props = {
 
 export const AppImage: FC<Props> = ({ className, id, to }) => {
   return to ? (
-    <Link
-      className={cn(
-        'relative rounded-md hover:after:opacity-100 after:content-[""] after:pointer-events-none after:opacity-0 after:inset-0 after:absolute after:bg-black/20 after:transition-opacity',
-        className
-      )}
-      to={to}
-    >
-      <img className="rounded-md" src={`https://steamcdn-a.akamaihd.net/steam/apps/${id}/header.jpg`} />
+    <Link className={cn(styles.appImage, className)} to={to}>
+      <img src={`https://steamcdn-a.akamaihd.net/steam/apps/${id}/header.jpg`} />
     </Link>
   ) : (
-    <img className={cn('rounded-md', className)} src={`https://steamcdn-a.akamaihd.net/steam/apps/${id}/header.jpg`} />
+    <img
+      className={cn(styles.appImage, className)}
+      src={`https://steamcdn-a.akamaihd.net/steam/apps/${id}/header.jpg`}
+    />
   );
 };
