f5d4f5f6584312eefa194f9d52d5402eea7e35c6
Cecilia Sanare <ceci@sanare.dev>
2025-03-02 14:01:39 -0600

    feat: swap to browser router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: '/',
     lazy: () => import('./App'),
