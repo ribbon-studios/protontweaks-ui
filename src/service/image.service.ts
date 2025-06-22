8cb4f460ec3356a9886e2f4604ecb57ad6ad445c
Cecilia Sanare <ceci@sanare.dev>
2025-06-22 15:49:54 -0500

    fix: more gracefully handle image preloading failures

--- src/service/image.service.ts
+++ src/service/image.service.ts
@@ -4,14 +4,21 @@ export class ImageService {
       srcs.map((src) => {
         const image = new Image();
 
-        const promise = new Promise<void>((resolve, reject) => {
+        const promise = new Promise<void>((resolve) => {
           image.addEventListener('load', () => resolve(), {
             once: true,
           });
 
-          image.addEventListener('error', reject, {
-            once: true,
-          });
+          image.addEventListener(
+            'error',
+            (event) => {
+              console.error(`Failed to load image from "${src}"!`);
+              resolve();
+            },
+            {
+              once: true,
+            }
+          );
         });
 
         // Set the SRC here to prevent race conditions
