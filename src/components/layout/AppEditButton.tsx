738c664a88479da07f2bf629c7146b2df38536a9
Cecilia Sanare <ceci@sanare.dev>
2025-07-04 12:11:39 -0500

    feat: simplify adding new apps a little

--- src/components/layout/AppEditButton.tsx
+++ src/components/layout/AppEditButton.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react';
+import { useMemo, type FC } from 'react';
 import { Edit } from 'lucide-react';
 import { Button } from '../common/Button';
 
@@ -7,15 +7,43 @@ type Props = {
 };
 
 export const AppQuickEdit: FC<Props> = ({ id }) => {
+  const to = useMemo(() => {
+    if (id) {
+      return `https://github.com/ribbon-studios/protontweaks-db/edit/main/apps/${id}.json`;
+    }
+
+    const url = new URL('https://github.com/ribbon-studios/protontweaks-db/new/main/apps');
+    url.searchParams.set('filename', '<app-id>.json');
+    url.searchParams.set(
+      'value',
+      JSON.stringify(
+        {
+          id: '<app-id>',
+          name: '<app-name>',
+          tweaks: {
+            tricks: [],
+            env: {},
+            args: [],
+            settings: {
+              gamemode: true,
+              mangohud: true,
+            },
+            system: {
+              gpu_driver: {},
+            },
+          },
+          issues: [],
+        },
+        undefined,
+        2
+      )
+    );
+
+    return url.toString();
+  }, [id]);
+
   return (
-    <Button
-      className="flex-shrink-0 bg-accent"
-      to={
-        id
-          ? `https://github.com/ribbon-studios/protontweaks-db/edit/main/apps/${id}.json`
-          : 'https://github.com/ribbon-studios/protontweaks-db/tree/main/apps'
-      }
-    >
+    <Button className="flex-shrink-0 bg-accent" to={to}>
       <Edit />
     </Button>
   );
